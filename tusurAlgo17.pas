552062bed50b33d08b1397976d40b7f66e8a9026
Ellaylone <heamik91@yandex.ru>
2014-11-13 23:40:15 +0500

    fresh start
    создал типы данных для списка инциденции

--- tusurAlgo17.pas
+++ tusurAlgo17.pas
@@ -1,4 +1,18 @@
 Program lr7;
+type
+	PNode = ^Node; {Соединенная вершина}
+	Node = record
+		Data : integer; {Номер вершины}
+		Next : PNode; {Ссылка на след соединенную вершину}
+	end;
+	PDot = ^Dot; {Вершина}
+	Dot = record
+		Data : integer; {Номер вершины}
+		Next : PDot; {Следующая вершина}
+		Prev : PDot; {Предыдущая вершина}
+		Node : PNode; {Ссылка на первую вершину с которой есть ребро}
+	end;
+
 begin
 	writeln('Done!');
 	readln();
