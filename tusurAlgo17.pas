247610e8e89f6d72bf8df524d63a5df634f5de75
Ellaylone <heamik91@yandex.ru>
2014-11-17 21:44:42 +0500

    add graf for testing
    added testing graf
    started findEiler procedure
    made sure that PNode and PDot structure actually works

--- tusurAlgo17.pas
+++ tusurAlgo17.pas
@@ -10,10 +10,63 @@ type
 		Data : integer; {Номер вершины}
 		Next : PDot; {Следующая вершина}
 		Prev : PDot; {Предыдущая вершина}
-		Node : PNode; {Ссылка на первую вершину с которой есть ребро}
+		Node : PNode; {Вершины с которыми есть ребра}
+                FirstNode : PNode; {Ссылка на первую вершину с которой есть ребро}
 	end;
+var
+  Dots, First : PDot;
 
+procedure addGraf;
+var
+     Temp : PDot;
 begin
-	writeln('Done!');
-	readln();
+     new(Dots);
+
+     First := Dots;
+     Dots^.Data := 1;
+     Dots^.Prev := nil;
+     new(Dots^.Node);
+     Dots^.Node^.Data := 2;
+     Dots^.FirstNode := Dots^.Node;
+     new(Dots^.Node^.Next);
+     Dots^.Node := Dots^.Node^.Next;
+     Dots^.Node^.Data := 3;
+
+     new(Dots^.Next);
+     Temp := Dots;
+     Dots := Dots^.Next;
+     Dots^.Data := 2;
+     Dots^.Prev := Temp;
+     new(Dots^.Node);
+     Dots^.Node^.Data := 1;
+     Dots^.FirstNode := Dots^.Node;
+     new(Dots^.Node^.Next);
+     Dots^.Node := Dots^.Node^.Next;
+     Dots^.Node^.Data := 3;
+
+     new(Dots^.Next);
+     Temp := Dots;
+     Dots := Dots^.Next;
+     Dots^.Data := 3;
+     Dots^.Prev := Temp;
+     new(Dots^.Node);
+     Dots^.Node^.Data := 1;
+     Dots^.FirstNode := Dots^.Node;
+     new(Dots^.Node^.Next);
+     Dots^.Node := Dots^.Node^.Next;
+     Dots^.Node^.Data := 2;
+     Dots^.Next := nil;
+end;
+procedure findEiler;
+begin
+     writeln(First^.Data);
+     writeln(First^.Node^.Data);
+     writeln(Dots^.Data);
+     writeln('find');
+end;
+begin
+     addGraf();
+     findEiler();
+     writeln('Done!');
+     readln();
 end.
